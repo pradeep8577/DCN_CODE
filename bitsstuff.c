a8a5d4057b26334c95a78b4878e848cdb5451966
Pradeep Pawar <72540482+pradeep8577@users.noreply.github.com>
2022-01-08 01:13:50 +0530

    Add files via upload

---
+++ bitsstuff.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+ int i,j=0,k=0,len;
+ char arr[50],src[50];
+ printf("Enter the data  string: ");
+ scanf("%s",arr);
+ len=strlen(arr);
+ 
+ for(i=0;i<len;i++)
+ {
+  if(arr[i]=='1')
+   {
+     k++;
+   }
+   else
+   {
+     k=0;
+   }
+   src[j]=arr[i];
+   j++;
+   if(k==5)
+   {
+     src[j]='0';
+     j++;
+     k=0;
+   }
+ }
+ src[j]='\0';
+ printf("output %s\n",src);
+ return 0;
+} 
+
